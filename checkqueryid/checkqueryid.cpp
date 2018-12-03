292b269667b71bebb098466fb33e6cf8f466fb6f
Riccardo Persiani <s225289@studenti.polito.it>
2018-12-03 17:38:28 +0000

    fix indentation eosusdprice

--- checkqueryid/checkqueryid.cpp
+++ checkqueryid/checkqueryid.cpp
@@ -8,31 +8,31 @@ class checkqueryid : public eosio::contract
   public:
     using contract::contract;
     
-   checkqueryid(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds) {}
+    checkqueryid(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds) {}
 
     [[eosio::action]] 
     void checkquery() 
-	{
-      capi_checksum256 myQueryId = oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=EOSUSD).result.EOSUSD.l.0");
-      oraclize_queryId_localEmplace(myQueryId);
-      print("Oraclize query was sent & queryId saved in a tbl record, standing by for the answer..");
+    {
+        capi_checksum256 myQueryId = oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=EOSUSD).result.EOSUSD.l.0");
+        oraclize_queryId_localEmplace(myQueryId);
+        print("Oraclize query was sent & queryId saved in a tbl record, standing by for the answer..");
     }
 
     [[eosio::action]]
     void callback(capi_checksum256 queryId, std::vector<unsigned char> result, std::vector<unsigned char> proof) 
-	{
-      require_auth(oraclize_cbAddress());
+    {
+        require_auth(oraclize_cbAddress());
       
-      if (!oraclize_queryId_match(queryId))
-      {
-        print("UNEXPECTED QUERY ID");
-      } 
-      else 
-      {      		
-        print("QueryId: ", checksum256_to_string(queryId));
-        const std::string result_str = vector_to_string(result);
-        print(" Result: ", result_str);
-      }
+        if (!oraclize_queryId_match(queryId))
+        {
+            print("UNEXPECTED QUERY ID");
+        } 
+        else 
+        {      		
+            print("QueryId: ", checksum256_to_string(queryId));
+            const std::string result_str = vector_to_string(result);
+            print(" Result: ", result_str);
+        }
     }
 };
 
--- eosusdprice/eosusdprice.cpp
+++ eosusdprice/eosusdprice.cpp
@@ -12,7 +12,7 @@ class eosusdprice : public eosio::contract
 
       [[eosio::action]]
       void execquery()
-	  {
+      {
           print("Sending query to Oraclize...");
           oraclize_query("URL", "json(https://min-api.cryptocompare.com/data/price?fsym=EOS&tsyms=USD).USD",\
            (proofType_Android | proofStorage_IPFS));
@@ -22,7 +22,8 @@ class eosusdprice : public eosio::contract
       void callback( checksum256 queryId, std::vector<uint8_t> result, std::vector<uint8_t> proof )
       {
           require_auth(oraclize_cbAddress());
-          const std::string result_str = vector_to_string(result);
+          
+		  const std::string result_str = vector_to_string(result);
           print("Result: ", result_str);
           print(" Proof length: ", proof.size());
       }
