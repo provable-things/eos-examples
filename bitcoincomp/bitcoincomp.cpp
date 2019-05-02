da97a87defbc02424508b2d33945cd033501a42e
Riccardo Persiani <r.persiani92@gmail.com>
2019-05-02 12:27:12 +0100

    feat: update eos-examples compatible with CDT 1.6.1

---
+++ bitcoincomp/bitcoincomp.cpp
@@ -0,0 +1,40 @@
+#include "oraclize/eos_api.hpp"
+
+class bitcoincomp : public eosio::contract
+{
+  public:
+    using contract::contract;
+
+    bitcoincomp(eosio::name receiver, eosio::name code, datastream<const char*> ds) : contract(receiver, code, ds) {}
+
+    [[eosio::action]]
+    void compute()
+    {
+        // Prepare computation arguments: IPFS Multihash and Bitcoin Address
+        std::string bitcoinAddress = "3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r";
+        std::vector<std::vector<unsigned char>> args = {
+             string_to_vector("QmYe37uvAUvZZ8ksV726BZt6dJFWP764sTPisNQtuDZVom"),
+             string_to_vector(bitcoinAddress)
+        };
+        std::vector<unsigned char> query = provable_set_computation_args(args);
+        // Send Provable query
+        print("Sending query to Oraclize...");
+        oraclize_query("computation", query);
+    }
+
+    [[eosio::action]]
+    void callback(
+        const std::string queryId,
+        const std::vector<unsigned char> result,
+        const std::vector<unsigned char> proof
+    )
+    {
+         // Check that Provable is the callback action caller
+         require_auth(provable_cbAddress());
+         // Print the account balance of the Bitcoin account
+         const std::string result_str = vector_to_string(result);
+         print("Account balance:", result_str);
+    }
+};
+
+EOSIO_DISPATCH(bitcoincomp, (compute)(callback))
--- randomsample/randomsample.cpp
+++ randomsample/randomsample.cpp
@@ -1,16 +1,14 @@
 #define ORACLIZE_NETWORK_NAME "eosio_testnet_jungle"
+#define CONTRACT_NAME "randomsample"
 
 #include "oraclize/eos_api.hpp"
-
-using namespace eosio;
-
 
 class randomsample : public eosio::contract
 {
   public:
     using contract::contract;
 
-    randomsample(name receiver, name code, datastream<const char*> ds) : contract(receiver, code, ds) {}
+    randomsample(eosio::name receiver, eosio::name code, datastream<const char*> ds) : contract(receiver, code, ds) {}
 
     [[eosio::action]]
     void getrandnum()
@@ -20,22 +18,23 @@ class randomsample : public eosio::contract
         uint32_t delay = 10;
         oraclize_newRandomDSQuery(delay, N);
     }
-    
-    [[eosio::action]]
-    void callback(capi_checksum256 queryId, std::vector<unsigned char> result, std::vector<unsigned char> proof)
-    {
-        require_auth(oraclize_cbAddress());
 
+    [[eosio::action]]
+    void callback(
+        const std::string queryId,
+        const std::vector<unsigned char> result,
+        const std::vector<unsigned char> proof
+    )
+    {
+        require_auth(provable_cbAddress());
         if (oraclize_randomDS_proofVerify(queryId, result, proof, _self) != 0)
         {
             // The proof verification has failed, manage this use case...
+            print(" Proof failed has failed...");
         }
         else
         {
-            uint8_t result_int = 0;
-            std::memcpy(&result_int, &result[0], result.size());
-            print("Number: ");
-            printi(result_int);
+            print("Number: ", result[0]);
         }
     }
 };
